f296468a7089038cf7a6dbea38d679fbcb83f15d
FrancescoLonighi <francesco.lonighi@galileiostiglia.edu.it>
2024-09-30 20:32:15 +0000

    02_01-10-24.c

---
+++ Informatica/02_01-10-24.c
@@ -0,0 +1,17 @@
+/*Dati 3 numeri stampare il più piccolo, il più grande e la loro media aritmetica*/
+#include <stdio.h>
+int main()
+{
+    int A,B,C;
+    A=0;
+    B=0;
+    C=0;
+    printf("inserisci un valore A: ");
+    scanf("%d", &A);
+    printf("inserisci un valore B: ");
+    scanf("%d", &B);
+    printf("inserisci un valore C: ");
+    scanf("%d", &C);
+    
+
+}
