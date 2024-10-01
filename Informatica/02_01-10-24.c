ff6812f5acf5ae7c94eb87b3c36db841b48c9890
FrancescoLonighi <francesco.lonighi@galileiostiglia.edu.it>
2024-10-01 07:54:48 +0000

    02_01-10-24

--- Informatica/02_01-10-24.c
+++ Informatica/02_01-10-24.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 int main()
 {
-    int A,B,C;
+    float A,B,C,max,min,media;
     A=0;
     B=0;
     C=0;
-    printf("inserisci un valore A: ");
-    scanf("%d", &A);
-    printf("inserisci un valore B: ");
-    scanf("%d", &B);
-    printf("inserisci un valore C: ");
-    scanf("%d", &C);
+    max=0
+    min=0
+    media=0
+    printf("inserisci tre valori: ");
+    scanf("%f", &A, &B, &C);
+    
     
 
 }
