6b1f4255089bb40adfcdba3f85bab343036942f4
FrancescoLonighi <francesco.lonighi@galileiostiglia.edu.it>
2024-11-18 17:56:46 +0000

    Lonighi_ES2.c

--- Informatica/Lonighi_ES3.c
+++ Informatica/Lonighi_ES3.c
@@ -7,47 +7,7 @@ AVVICINA DI PIÙ;
 4) DETERMINARE SE UNO È MULTIPLO DELL’ALTRO.*/
 #include <stdio.h>
 int main(){
-    int num1, num2, somma, K, maggiore, minore, differenza, scelta;
-    num1=0;
-    num2=0;
-    K=0;
     
-    printf("inserisci un primo numero: ");
-    scanf("%d", &num1);
-    printf("inserisci un secondo numero: ");
-    scanf("%d", &num2);
-    printf("inserisci un valore a K: ");
-    scanf("%d", &K);
-
-    printf("\n1. somma in valore assoluto: ");
-    printf("\n2. differenza tra maggiore e minore");
-    printf("\n3. quale valore di avvicina di più: ");
-    printf("\n4. determiare se è un multiplo di dell'altro");
-    printf("\nscegli un valore dal menù: ");
-    scanf("%d", &scelta);
-    if(scelta>0 && scelta<0){
-        switch (scelta);
-        {
-        case 1:
-            printf("\nla somma assoluta vale: %.2d", num1+num2);
-            break;
-        case 2:
-            printf("\nla differenza vale: %.2d", num1-num2);
-            break;
-        case 3:
-            printf("\nvalore che si avvicina di più %.2d", );
-            break;
-        case 4:
-            printf("\nmultiplo rispetto all'altro %.2d", num1*num2);
-            break;
-        default:
-            printf("\nhai inserito una scelta non valida: ");
-        
-       
-        }
-
-    }
-
 
 
 
--- Informatica/Lonighi_ES4.c
+++ Informatica/Lonighi_ES4.c
@@ -2,14 +2,7 @@
 L’ESPONENTE.*/
 #include <stdio.h>
 int main(){
-    int num1;
-    printf("inserisci un valore da 1 a 20: ", num1);
-    scanf("%d", &num1);
-    if(num1/2){
-        printf("è una potenza di 2");
-    }
-    else 
-        printf("non è una potenza di 2");
+   
 
     
 
